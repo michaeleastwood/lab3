f4f61708f07f8b888b96f553ceb80f08b71b8361
dethuey <dthuey@studentbody.byu.edu>
2018-10-18 00:25:26 +0000

    Added page functionality

---
+++ script.js
@@ -0,0 +1,90 @@
+angular.module('game', ['ui.router'])
+    .factory('locFactory', [function() {
+        var o = {
+            loc: ''
+        };
+        return o;
+    }])
+    .factory('charFactory', [function() {
+        var o = {
+            char: ''
+        };
+        return o;
+    }])
+    .config([
+        '$stateProvider',
+        '$urlRouterProvider',
+        function($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('home', {
+                    url: '/home',
+                    templateUrl: '/home.html',
+                    controller: 'HomeCtrl'
+                })
+                .state('game', {
+                    url: '/game/',
+                    templateUrl: '/game.html',
+                    controller: 'GameCtrl'
+                });
+            $urlRouterProvider.otherwise('home');
+        }
+    ])
+    .controller('HomeCtrl', [
+        '$scope',
+        'locFactory',
+        'charFactory',
+        function($scope, locFactory, charFactory) {
+            $scope.map = [{
+                "shape": "rect",
+                "type": "New York City",
+                "coords": "82,85,150,150"
+            }, {
+                "shape": "circle",
+                "type": "Amazon",
+                "coords": "150,200,40"
+            }, {
+                "shape": "circle",
+                "type": "Europe",
+                "coords": "210,150,25"
+            }, {
+                "shape": "circle",
+                "type": "Africa",
+                "coords": "250, 200, 30"
+            }, {
+                "shape": "circle",
+                "type": "Russia",
+                "coords": "300,100,75"
+            }];
+            $scope.location = locFactory.loc;
+            $scope.character = charFactory.char;
+
+            // $scope.start = function() {
+            //     alert("here");
+            //     if ($scope.location === '' || $scope.character === '') { return; }
+            // //    locFactory.loc = $scope.location;
+            //   //  charFactory.character = $scope.character;
+            // };
+            $scope.partClicked = function(id) {
+                $scope.location = id;
+                locFactory.loc = id;
+                console.log(locFactory);
+            };
+        }
+    ])
+    .controller('GameCtrl', [
+        '$scope',
+        'locFactory',
+        'charFactory',
+        function($scope, locFactory, charFactory) {
+            console.log(locFactory);
+            $scope.location = locFactory.loc;
+            switch($scope.location){
+                case "New York City":
+                    $scope.image = "grass.png"
+            }
+            console.log($scope.image);
+            // $scope.loc = locFactory.locations[$stateParams.id];
+            // $scope.addComment = function() {
+            // };
+        }
+    ]);
