36ae8bf87cfbb0d85ef561fea0c3683633efdb58
michaeleastwood <43073347+michaeleastwood@users.noreply.github.com>
2018-10-18 22:37:18 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var buttonArr = []
+window.addEventListener("keydown", keydown);
+
+window.addEventListener("keyup", keyup);
+
 angular.module('game', ['ui.router'])
     .factory('locFactory', [function() {
         var o = {
@@ -54,12 +59,59 @@ angular.module('game', ['ui.router'])
                 "shape": "circle",
                 "type": "Russia",
                 "coords": "300,100,75"
+            }, {
+                "shape": "circle",
+                "type": "Ocean",
+                "coords": "10,10,30"
             }];
             $scope.characters = [{
-                "id" : "",
-                "src" : "",
-                "height" : "",
-                "width" : ""
+                id: "Sam",
+                spriteWidth: 800,
+                spriteHeight: 400,
+                rows: 2,
+                cols: 4,
+                trackRight: 0,
+                trackLeft: 1,
+                reverseRow: false,
+                pauseFrame: 0,
+                startHeight: 400,
+                src: "sprite.png"
+            }, {
+                id: "James",
+                spriteWidth: 512,
+                spriteHeight: 254,
+                rows: 2,
+                cols: 4,
+                trackRight: 0,
+                trackLeft: 1,
+                reverseRow: false,
+                pauseFrame: 0,
+                startHeight: 450,
+                src: "sprite2.png"
+            }, {
+                id: "Geran",
+                spriteWidth: 2048,
+                spriteHeight: 256,
+                rows: 2,
+                cols: 8,
+                trackRight: 0,
+                trackLeft: 1,
+                reverseRow: true,
+                pauseFrame: 4,
+                startHeight: 450,
+                src: "puma.png"
+            }, {
+                id: "Kevin",
+                spriteWidth: 650,
+                spriteHeight: 431,
+                rows: 2,
+                cols: 4,
+                trackRight: 0,
+                trackLeft: 1,
+                reverseRow: false,
+                pauseFrame: 0,
+                startHeight: 400,
+                src: "sprite3.png"
             }]
             $scope.location = locFactory.loc;
             $scope.character = charFactory.char;
@@ -86,10 +138,195 @@ angular.module('game', ['ui.router'])
         'charFactory',
         function($scope, locFactory, charFactory) {
             $scope.location = locFactory.loc;
-            switch($scope.location){
+            switch ($scope.location) {
                 case "New York City":
-                    $scope.image = "grass.png"
+                    $scope.image = {
+                        width: 1920,
+                        height: 1080,
+                        src: "newyork.png"
+                    }
+                    break;
+                case "Russia":
+                    $scope.image = {
+                        width: 1920,
+                        height: 1439,
+                        src: "russia.png"
+                    }
+                    break;
+                case "Ocean":
+                    $scope.image = {
+                        width: 1920,
+                        height: 960,
+                        src: "water.png"
+                    }
+                    break;
+                case "Amazon":
+                    $scope.image = {
+                        width: 1920,
+                        height: 1080,
+                        src: "jungle.png"
+                    }
+                    break;
+                case "Africa":
+                    $scope.image = {
+                        width: 1920,
+                        height: 1152,
+                        src: "desert.png"
+                    }
+                    break;
+                case "Europe":
+                    $scope.image = {
+                        width: 1920,
+                        height: 1447,
+                        src: "beach.png"
+                    }
+                    break;
             }
+
+            console.log($scope.image)
+            console.log(charFactory.char)
+            startGame(charFactory.char, $scope.image)
 
         }
     ]);
+
+function startGame(character, bg) {
+    var canvasWidth = 1300;
+    var canvasHeight = 600;
+
+    var spriteWidth = character.spriteWidth;
+    var spriteHeight = character.spriteHeight;
+
+    var pauseFrame = character.pauseFrame;
+
+    var rows = character.rows;
+    var cols = character.cols;
+
+    var trackRight = character.trackRight;
+    var trackLeft = character.trackLeft;
+
+    var width = spriteWidth / cols;
+    var height = spriteHeight / rows;
+
+    var curFrame = 0;
+    var frameCount = character.cols;
+
+    var x = 0;
+    var y = character.startHeight;
+
+    var srcX;
+    var srcY;
+
+
+
+    var speed = 24;
+
+    var canvas = document.getElementById('canvas');
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    var ctx = canvas.getContext("2d");
+
+    var sprite = new Image();
+    sprite.src = character.src;
+    var background = new Image();
+    background.src = bg.src
+    var bgsrcX = 0;
+
+    function updateFrame() {
+
+        if (buttonArr.includes('a') && buttonArr.includes('d')) {
+            curFrame = ++curFrame % frameCount;
+            srcX = curFrame * width;
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+            srcX = pauseFrame * width;
+        }
+        else if (buttonArr.includes('a')) {
+            if (character.reverseRow) {
+                if (curFrame == 0) {
+                    curFrame = frameCount - 1;
+                }
+                else {
+                    curFrame = --curFrame % frameCount;
+                }
+            }
+            else {
+                curFrame = ++curFrame % frameCount;
+            }
+            srcX = curFrame * width;
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+            srcY = trackLeft * height;
+            if (x > 0) {
+                x -= speed;
+            }
+        }
+        else if (buttonArr.includes('d')) {
+            curFrame = ++curFrame % frameCount;
+            srcX = curFrame * width;
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+            srcY = trackRight * height;
+            if (x < (3 * canvasWidth / 4) - width) {
+                x += speed;
+            }
+        }
+        else {
+            curFrame = ++curFrame % frameCount;
+            srcX = curFrame * width;
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+            srcX = pauseFrame * width;
+        }
+    }
+
+    function updateBackground() {
+
+        if (x >= (3 * canvasWidth / 4) - width && buttonArr.includes('d') && !buttonArr.includes('a')) {
+
+            bgsrcX += 20
+            if (bgsrcX > (bg.width - canvasWidth) && bgsrcX < bg.width) {
+                ctx.drawImage(background, 0, 0, canvasWidth, bg.height, bg.width - bgsrcX, 0, canvasWidth, canvasHeight);
+            }
+            else if (bgsrcX >= bg.width) {
+                bgsrcX = 0
+            }
+            ctx.drawImage(background, bgsrcX, 0, canvasWidth, bg.height, 0, 0, canvasWidth, canvasHeight);
+        }
+        else {
+            if (bgsrcX > (bg.width - canvasWidth) && bgsrcX < bg.width) {
+                ctx.drawImage(background, 0, 0, canvasWidth, bg.height, bg.width - bgsrcX, 0, canvasWidth, canvasHeight);
+            }
+            else if (bgsrcX >= bg.width) {
+                bgsrcX = 0
+            }
+            ctx.drawImage(background, bgsrcX, 0, canvasWidth, bg.height, 0, 0, canvasWidth, canvasHeight);
+        }
+
+
+
+    }
+
+    function draw() {
+        updateFrame();
+        updateBackground();
+
+        ctx.drawImage(sprite, srcX, srcY, width, height, x, y, width, height);
+    }
+
+
+
+    setInterval(draw, 100);
+
+}
+
+
+
+
+function keyup(event) {
+    buttonArr.splice(buttonArr.indexOf(event.key.toLowerCase()), 1)
+}
+
+function keydown(event) {
+    console.log(event.key)
+    if (!buttonArr.includes(event.key.toLowerCase())) {
+        buttonArr.push(event.key.toLowerCase());
+    }
+}
--- spaceship.js
+++ spaceship.js
@@ -7,34 +7,68 @@ var purpleSuit = {
     cols: 4,
     trackRight: 0,
     trackLeft: 1,
+    reverseRow: false,
+    pauseFrame: 0,
+    startHeight: 200,
     url: "sprite.png"
 };
 var greenShirt = {
     name: "James",
-    spriteWidth: 449,
+    spriteWidth: 512,
     spriteHeight: 254,
     rows: 2,
     cols: 4,
     trackRight: 0,
     trackLeft: 1,
+    reverseRow: false,
+    pauseFrame: 0,
+    startHeight: 250,
     url: "sprite2.png"
 };
+var puma = {
+    name: "Geran",
+    spriteWidth: 2048,
+    spriteHeight: 256,
+    rows: 2,
+    cols: 8,
+    trackRight: 0,
+    trackLeft: 1,
+    reverseRow: true,
+    pauseFrame: 4,
+    startHeight: 250,
+    url: "puma.png"
+};
 
+var jungle  = {
+    width: 1920,
+    height: 1080,
+    url: "jungle.png"
+}
+var newyork = {
+    width: 1920,
+    height: 1080,
+    url: "newyork.png"
+}
+
+var bg = jungle;
 angular.module('characterSelection', [])
     .controller("CharacterSelect", ['$scope', function($scope) {
-        
-        $scope.characters = [ purpleSuit, greenShirt ];
-        $scope.go = { selection: purpleSuit.name };
+
+        $scope.characters = [purpleSuit, greenShirt, puma];
+        $scope.go = { selection: purpleSuit };
+        $scope.start = function(character) {
+            startGame(character, bg)
+        }
     }]);
 
-function startGame(character) {
-    var canvasWidth = 800;
+function startGame(character, bg) {
+    var canvasWidth = 1200;
     var canvasHeight = 400;
 
     var spriteWidth = character.spriteWidth;
     var spriteHeight = character.spriteHeight;
 
-    var pauseFrame = 0;
+    var pauseFrame = character.pauseFrame;
 
     var rows = character.rows;
     var cols = character.cols;
@@ -46,10 +80,10 @@ function startGame(character) {
     var height = spriteHeight / rows;
 
     var curFrame = 0;
-    var frameCount = 4;
+    var frameCount = character.cols;
 
     var x = 0;
-    var y = 200;
+    var y = character.startHeight;
 
     var srcX;
     var srcY;
@@ -64,39 +98,87 @@ function startGame(character) {
 
     var ctx = canvas.getContext("2d");
 
-    var character = new Image();
-    character.src = character.url;
+    var sprite = new Image();
+    sprite.src = character.url;
     var background = new Image();
-    background.src = "grass.png"
-
+    background.src = bg.url
+    var bgsrcX = 0;
     function updateFrame() {
-        curFrame = ++curFrame % frameCount;
-        srcX = curFrame * width;
-        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
         if (buttonArr.includes('a') && buttonArr.includes('d')) {
+            curFrame = ++curFrame % frameCount;
+            srcX = curFrame * width;
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
             srcX = pauseFrame * width;
         }
         else if (buttonArr.includes('a')) {
+            if (character.reverseRow) {
+                if (curFrame == 0) {
+                    curFrame = frameCount - 1;
+                }
+                else {
+                    curFrame = --curFrame % frameCount;
+                }
+            }
+            else {
+                curFrame = ++curFrame % frameCount;
+            }
+            srcX = curFrame * width;
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
             srcY = trackLeft * height;
             if (x > 0) {
                 x -= speed;
             }
         }
         else if (buttonArr.includes('d')) {
+            curFrame = ++curFrame % frameCount;
+            srcX = curFrame * width;
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
             srcY = trackRight * height;
             if (x < (3 * canvasWidth / 4) - width) {
                 x += speed;
             }
         }
         else {
+            curFrame = ++curFrame % frameCount;
+            srcX = curFrame * width;
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
             srcX = pauseFrame * width;
         }
     }
 
+    function updateBackground() {
+        
+        if (x >= (3 * canvasWidth / 4) - width && buttonArr.includes('d') && !buttonArr.includes('a')){
+            
+            bgsrcX += 20
+            if (bgsrcX > (bg.width - canvasWidth) && bgsrcX < bg.width){
+                ctx.drawImage(background, 0, 0, canvasWidth,bg.height, bg.width-bgsrcX, 0, canvasWidth, canvasHeight);
+            }
+            else if(bgsrcX >= bg.width){
+                bgsrcX = 0
+            }
+            ctx.drawImage(background, bgsrcX, 0, canvasWidth,bg.height, 0, 0, canvasWidth, canvasHeight);
+        }
+        else{
+            if (bgsrcX > (bg.width - canvasWidth) && bgsrcX < bg.width){
+                ctx.drawImage(background, 0, 0, canvasWidth,bg.height, bg.width-bgsrcX, 0, canvasWidth, canvasHeight);
+            }
+            else if(bgsrcX >= bg.width){
+                bgsrcX = 0
+            }
+            ctx.drawImage(background, bgsrcX, 0, canvasWidth,bg.height, 0, 0, canvasWidth, canvasHeight);
+        }
+
+
+
+    }
+
     function draw() {
         updateFrame();
-        ctx.drawImage(background, 0, 0, 1920, 1080, 0, 0, 800, 400);
-        ctx.drawImage(character, srcX, srcY, width, height, x, y, width, height);
+        updateBackground();
+
+        ctx.drawImage(sprite, srcX, srcY, width, height, x, y, width, height);
     }
 
 
